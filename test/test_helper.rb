fabde334cbfaff9fd3790067698eda79cc3d761b
padeen <paulo.deen@gmail.com>
2017-09-15 01:46:06 +0200

    Create pdf delivery test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+require 'rails/test_unit/reporter'
 require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
@@ -5,7 +6,7 @@ require "rails/test_help"
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-
+ 
 Rails::TestUnitReporter.executable = 'bin/test'
 
 # Load fixtures from the engine
